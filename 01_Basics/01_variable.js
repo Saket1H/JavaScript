a6f06d5742994265532819e8b4d67c37d0d90da0
Saket1H <98052016+Saket1H@users.noreply.github.com>
2023-12-14 15:00:58 +0000

    let,const,var

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=23423
+let accountemail="saket13@google.com"
+var accountpassword="alpha"
+accountcity="Delhi"
+let accountstate
+
+//accountId=2 --> Not Allowed
+
+accountemail="hs@ps.com"
+accountpassword="beta"
+accoundcity="Bangalore"
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountemail,accountpassword,accoundcity,accountstate])
