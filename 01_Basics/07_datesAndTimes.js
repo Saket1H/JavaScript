fce4967b1c6d306e2b0824407e5c2138af464539
SAKET KUMAR <98052016+Saket1H@users.noreply.github.com>
2023-12-16 04:11:09 +0000

    Dates and Times

---
+++ 01_Basics/07_datesAndTimes.js
@@ -0,0 +1,32 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate); //object
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);//give the time in millisecond after 1970
+
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));//to convert millisecond into seconds
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());  //1->Monday 7->Sunday
+
+// `${newDate.getDay()} and the time `
+
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",    
+}))//print-> Sunday, Monday
