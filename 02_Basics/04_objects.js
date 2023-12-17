478271fceefb34fccb7b66fa9407d833ca11cb86
SAKET KUMAR <98052016+Saket1H@users.noreply.github.com>
2023-12-17 16:25:01 +0000

    json talks and object destructoring

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -1,4 +1,4 @@
-// const tinderUser = new Object()->In this way we can also declare the objects
+// const tinderUser = new Object()
 const tinderUser = {}
 
 tinderUser.id = "123abc"
@@ -11,8 +11,8 @@ const regularUser = {
     email: "some@gmail.com",
     fullname: {
         userfullname: {
-            firstname: "Saket",
-            lastname: "Kumar"
+            firstname: "hitesh",
+            lastname: "choudhary"
         }
     }
 }
@@ -21,12 +21,12 @@ const regularUser = {
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
-const obj3 = {5: "a", 6: "b"}
+const obj4 = {5: "a", 6: "b"}
 
-// const obj4 = { obj1, obj2 }  //for concating
-// const obj4 = Object.assign({}, obj1, obj2, obj4), // used for concating.
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
 
-const obj4 = {...obj1, ...obj2, ...obj3}
+const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
 
@@ -48,11 +48,11 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));//give the array of keys
-// console.log(Object.values(tinderUser));//give the array of values
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));// that the particular property exists or not.
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
 const course = {
@@ -60,3 +60,22 @@ const course = {
     price: "999",
     courseInstructor: "hitesh"
 }
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
